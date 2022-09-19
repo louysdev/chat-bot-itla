f41a2afeba4f5aaa6c777c81024dfc876968249c
Louys <louysdev@gmail.com>
2022-09-18 20:23:35 -0400

    Subiendo proyecto a un repositorio

---
+++ questions.py
@@ -0,0 +1,57 @@
+# Preguntas y repuestas del chatbot para atender solicitudes del ITLA
+
+
+data = {'clients': []}
+
+data['clients'].append({
+    'responseBot': 'Representante Jose Ventura le asiste, un placer, indique su problemática',
+    'listWords': ["buenas", "buen", "hello", "hola", "saludos", "dias", "tardes", "buenos"]})
+
+data['clients'].append({
+    'responseBot': 'Trabajamos de Lunes a Viernes de 8 AM a 4 PM',
+    'listWords': ["cual", "horario", "trabajo", "jornada", "atencion"]})
+
+data['clients'].append({
+    'responseBot': 'Se dirige a la página web orbi.do y allí hace su proceso de registro',
+    'listWords': ["como", "solicitar", "admisiones", "puedo", "hacer", "admision"]})
+
+data['clients'].append({
+    'responseBot': 'En la sección de solicitudes>retiros en la plataforma Orbi',
+    'listWords': ["como", "puedo", "retirar", "materia", "quiero", "asignatura"]})
+
+data['clients'].append({
+    'responseBot': 'El conto de inscripción y reinscripción son 6,640 pesos',
+    'listWords': ["cual", "precio", "inscripcion", "reinscripcion", "es", "monto", "cuanto"]})
+
+data['clients'].append({
+    'responseBot': 'El precio de cada crédito es de 520 pesos',
+    'listWords': ["cuanto", "precio", "credito", "creditos", "cada", "cual"]})
+
+data['clients'].append({
+    'responseBot': 'Los pagos los realiza a través de la sección caja>Pagos en línea en la Plataforma Orbi',
+    'listWords': ["como", "realizar", "realizo", "pago", "pagos", "donde", "puedo"]})
+
+data['clients'].append({
+    'responseBot': "Para reportar un maestro se debe comunicar con el encargado de la carrera, ejemplo el Centro "
+                   "Excelencia Software",
+    'listWords': ["como", "reportar", "profesor", "donde", "maestro", "docente"]})
+
+data['clients'].append({
+    'responseBot': 'Los cursos ofrecidos en educación permanente puede verlos desde nuestra web',
+    'listWords': ["cuales", "cursos", "ofrecen", "tienen", "educacion", "permanente"]})
+
+data['clients'].append({
+    'responseBot': 'Los métodos de pago que disponemos son transacción bancaria y pagos por tarjeta',
+    'listWords': ["cuales", "metodos", "metodo", "pago", "acepta", "aceptan", "cual", "usan"]})
+
+data['clients'].append({
+    'responseBot': 'Para solicitar el carnet del ITLA debe tirarse la foto en el edificio 1, el primer carnet es '
+                   'gratuito',
+    'listWords': ["como", "solicitar", "obtener", "carnet", "itla"]})
+
+data['clients'].append({
+    'responseBot': 'Para saber la condición de su solicitud de beca puede ir a la sección Beca>Ver Solicitud Beca',
+    'listWords': ["beca", "saber", "conocer", "solicitud", "como", "donde", "condicion", "proceso"]})
+
+
+
